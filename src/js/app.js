38026155a2acff418a53b5b7ae60eadecea9962a
Sindhura17 <sindhura.l.bhat@gmail.com>
2020-12-19 19:50:05 +0530

    metamask connection working

--- src/js/app.js
+++ src/js/app.js
@@ -100,14 +100,29 @@ window.App = {
 // When the page loads, we create a web3 instance and set a provider. We then set up the app
 window.addEventListener("load", function() {
   // Is there an injected web3 instance?
-  if (typeof web3 !== "undefined") {
-    console.warn("Using web3 detected from external source like Metamask")
-    // If there is a web3 instance(in Mist/Metamask), then we use its provider to create our web3object
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:7545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for deployment. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      window.ethereum.enable();
+      // Acccounts now exposed
+      window.web3= web3;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    console.log('Injected web3 detected.');
+
+  }
+  // Fallback to localhost; use dev console port by default...
+  else {
+    const provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+    const web3 = new Web3(provider);
+    console.log('No web3 instance injected, using Local web3.');
+    window.web3= web3;
   }
   // initializing the App
   window.App.start()
